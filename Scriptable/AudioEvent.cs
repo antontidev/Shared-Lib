bdb3740b2a9405e845b4db8efb7bfe637d067bbe
Anton <sanjaplushka@gmail.com>
2021-01-30 13:13:20 +0500

    Submodule update

---
+++ Scriptable/AudioEvent.cs
@@ -0,0 +1,22 @@
+﻿using SukharevShared;
+using UnityEngine;
+
+public abstract class AudioEvent : ScriptableObject {
+    public AudioClip[] clips;
+
+    public RangedFloat volume;
+
+    public RangedFloat pitch;
+
+    public abstract void Play(AudioSource source);
+
+    public void Stop(AudioSource source) {
+        source.Stop();
+    }
+
+    public void PlayLoop(AudioSource source) {
+        Play(source);
+
+        source.loop = true;
+    }
+}
---
+++ Scriptable/SimpleAudioEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(menuName = "AudioEvent/Simple")]
+public class SimpleAudioEvent : AudioEvent {
+    public override void Play(AudioSource source) {
+        if (clips.Length == 0)
+            return;
+
+        source.clip = clips[Random.Range(0, clips.Length)];
+
+        source.volume = Random.Range(volume.min, volume.max);
+        source.pitch = Random.Range(pitch.min, pitch.max);
+
+        source.Play();
+    }
+}
